66559e5e23429d6dcbf007a3330a96a08acca7b1
Kingsley Paul Grant <kingsleypaulgrant15@gmail.com>
2017-03-29 18:25:20 -0400

    updated

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '1gQKGfLxlHs6pT68pXXqJVZvt',
+  consumer_secret: 'Ij8JPehfEm5DomFjfqOHhE3a8FvFnRqBaPOCpxSrlIFtuoRXS9',
+  access_token_key: '846456783573057537-pfbN8QpTJbXP2xPKtnud0upcP8dyN1N',
+  access_token_secret: 'B6pOP7zxXpGjjUCcSjTfwKVMOzDAzxuFdBrLF3qprZRBv',
+}
---
+++ liri.js
@@ -0,0 +1,11 @@
+var request = require('request');
+var spotify = require('spotify');
+var twitter = require ('require')
+var fs = require('fs');
+
+
+var command = process.argv[2];
+var argument = process.argv[3];
+var output;
+
+
